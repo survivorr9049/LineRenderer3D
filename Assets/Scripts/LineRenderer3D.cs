650549220d78e7e6d37ad252745b89fe5c2c150e
survivorr <kangur20040@wp.pl>
2024-03-15 12:44:07 +0100

    remove unnecessary code

--- Assets/Scripts/LineRenderer3D.cs
+++ Assets/Scripts/LineRenderer3D.cs
@@ -95,10 +95,8 @@ public class LineRenderer3D : MonoBehaviour
     {
         meshRenderer.sharedMaterial = material;
         points.Clear();
-        Vector3 position = Vector3.zero;
-        for(int i = 0; i < 32; i++){
-            position += new Vector3(Random.Range(-15, 15), Random.Range(-15, 15), Random.Range(-15, 15)) * 0.1f;
-            points.Add(new Point(position, 0.3f));
+        for(float i = 0; i < 128; i++){
+            points.Add(new Point(transform.position + Vector3.up * Mathf.Cos(i / 3) + Vector3.right * Mathf.Sin(i / 5) + Vector3.forward * Mathf.Cos(i / 7    ), 0.1f));
         }
     }
 
@@ -157,6 +155,9 @@ public class LineRenderer3D : MonoBehaviour
             Vector3 direction = Vector3.Lerp(previous, next, 0.5f).normalized;
             Vector3 normal = (next - previous).normalized * Mathf.Abs(Vector3.Dot(previous, direction)); //length encodes cosine of angle   
             Vector3 right = Vector3.Cross(direction, Vector3.right).normalized;
+            if(Mathf.Approximately(0, right.magnitude)){
+                right = Vector3.Cross(direction, Vector3.forward).normalized;
+            }
             Vector3 up = Vector3.Cross(direction, right).normalized;
             points[i] = new Point(points[i].position, direction, normal, up, right, points[i].thickness);
 
@@ -171,22 +172,13 @@ public class LineRenderer3D : MonoBehaviour
         points[points.Count()-1] = new Point(points[points.Count()-1].position, edgeDirection, Vector3.zero, edgeUp, edgeRight, points[points.Count()-1].thickness); 
     
         for(int i = 0; i < points.Count(); i++){
-
             if (i == points.Count() - 1) continue;
             Vector3 fromTo = (points[i + 1].position - points[i].position).normalized;
             Vector3 firstRight = points[i].right - Vector3.Dot(points[i].right, fromTo) * fromTo;
             Vector3 secondRight = points[i+1].right - Vector3.Dot(points[i+1].right, fromTo) * fromTo;
-            Vector3 firstUp = points[i].up - Vector3.Dot(points[i].up, fromTo) * fromTo;
-            Vector3 secondUp = points[i+1].up - Vector3.Dot(points[i+1].up, fromTo) * fromTo;
-            float angleRight = -Mathf.Acos(Vector3.Dot(firstRight, secondRight));
-            float angleUp = -Mathf.Acos(Vector3.Dot(firstUp, secondUp));
-            float angle = Mathf.Lerp(angleRight, angleUp, 0.5f);
-            Quaternion rot = Quaternion.AngleAxis(angleRight * Mathf.Rad2Deg + rotation, points[i + 1].direction);
+            float angleRight = -Mathf.Acos(Vector3.Dot(firstRight, secondRight)) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.AngleAxis(angleRight + rotation, points[i + 1].direction);
             if(fixTwisting) points[i+1] = new Point(points[i+1].position, points[i+1].direction, points[i+1].normal, rot * points[i+1].up, rot * points[i+1].right, points[i+1].thickness);
-            Debug.DrawRay(points[i+1].position, points[i].up, Color.cyan);
-            Debug.DrawRay(points[i+1].position, points[i].right, Color.magenta);
-            Debug.DrawRay(points[i + 1].position, points[i + 1].direction, Color.green);
-            Debug.DrawRay(points[i + 1].position + transform.position, points[i + 1].normal, Color.black);
         }   
 
 
